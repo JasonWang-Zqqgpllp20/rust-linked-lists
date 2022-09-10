7c30fe48c9828c6a14a0f57fee550617a54d198f
Jason Wang <zqqgpllp20@gmail.com>
2022-09-11 02:40:44 +0800

    Implement Drop for List. When we recurve the list, we cannot mutate it while using Rc, so we need to check if the strong reference by using Rc::try_unwrap() to take the value out of Rc.

--- src/third.rs
+++ src/third.rs
@@ -41,6 +41,20 @@ impl<T> List<T> {
         }
     }
 }
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut head = self.head.take();
+        while let Some(node) = head {
+            if let Ok(mut node) = Rc::try_unwrap(node) {
+                head = node.next.take();
+            } else {
+                break;
+            }
+        }
+    }
+}
+
 // can't implement IntoIter or IterMut for this type
 pub struct Iter<'a, T> {
     next: Option<&'a Node<T>>,
